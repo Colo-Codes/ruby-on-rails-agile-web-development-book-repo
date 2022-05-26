9c00796fc2b62e19af2f29c089f5e076a4d5d8a7
Damian Demasi <damian@amplify-now.com>
2022-05-26 18:59:36 +0930

    Fast forward to Task C as I lost the previous config.

--- test/controllers/products_controller_test.rb
+++ test/controllers/products_controller_test.rb
@@ -1,10 +1,21 @@
-require "test_helper"
+#---
+# Excerpted from "Agile Web Development with Rails 6",
+# published by The Pragmatic Bookshelf.
+# Copyrights apply to this code. It may not be used to create training material,
+# courses, books, articles, and the like. Contact us if you are in doubt.
+# We make no guarantees that this code is fit for any purpose.
+# Visit http://www.pragmaticprogrammer.com/titles/rails6 for more book information.
+#---
+require 'test_helper'
 
 class ProductsControllerTest < ActionDispatch::IntegrationTest
   setup do
     @product = products(:one)
+    @title = "The Great Book #{rand(1000)}"
+
   end
 
+  
   test "should get index" do
     get products_url
     assert_response :success
@@ -15,14 +26,25 @@ class ProductsControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
   end
 
+  
   test "should create product" do
-    assert_difference("Product.count") do
-      post products_url, params: { product: { description: @product.description, image_url: @product.image_url, price: @product.price, title: @product.title } }
+    assert_difference('Product.count') do
+      
+    post products_url, params: { 
+        product: { 
+          description: @product.description, 
+          image_url: @product.image_url, 
+          price: @product.price, 
+          title: @title,
+        }
+      }
+    
     end
 
     assert_redirected_to product_url(Product.last)
   end
 
+  
   test "should show product" do
     get product_url(@product)
     assert_response :success
@@ -33,13 +55,24 @@ class ProductsControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
   end
 
+  
   test "should update product" do
-    patch product_url(@product), params: { product: { description: @product.description, image_url: @product.image_url, price: @product.price, title: @product.title } }
+    
+    patch product_url(@product), params: { 
+        product: { 
+          description: @product.description, 
+          image_url: @product.image_url, 
+          price: @product.price, 
+          title: @title,
+        }
+      }
+    
     assert_redirected_to product_url(@product)
   end
 
+  
   test "should destroy product" do
-    assert_difference("Product.count", -1) do
+    assert_difference('Product.count', -1) do
       delete product_url(@product)
     end
 
